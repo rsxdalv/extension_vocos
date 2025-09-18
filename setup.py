0e23d81a7fb356bbdfb404642d067b3f1d667bc2
Roberts Slisans <rsxdalv@gmail.com>
2025-09-18 13:40:08 +0300

    migrate to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
+from pathlib import Path
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text(encoding="utf-8") if (HERE / "README.md").exists() else ""
 
 setuptools.setup(
     name="tts_webui_extension.vocos",
     packages=setuptools.find_namespace_packages(),
-    version="0.0.2",
+    version="0.0.3",
     author="rsxdalv",
     description="Vocos is a neural audio codec for high-quality audio compression and reconstruction",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/rsxdalv/tts_webui_extension.vocos",
     project_urls={},
     scripts=[],
